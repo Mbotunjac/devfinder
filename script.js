d68e5b36470d0dae2d69eab147bac3869a3be494
Miljana Botunjac <miljanabotunjac@gmail.com>
2021-10-08 14:48:36 +0200

    finished

--- script.js
+++ script.js
@@ -1,190 +1,156 @@
+//variables
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const searchbar = document.querySelector('.form-control');
+const content = document.getElementById('content');
+const root = document.documentElement.style;
+const baseUrl = 'https://api.github.com/users/';
+const toggleDarkModeButton = document.querySelector('#toggle-dark-mode');
+const input = document.getElementById('search');
 
-// light-dark mode depending on system pref. 
+const noResults = document.getElementById('no-results');
+const toggleText = document.getElementById("toggle-text");
+const icon = document.getElementById("icon");
+const submit = document.getElementById('submit');
+const fullname = document.getElementById('name');
+const username = document.getElementById('username');
+const avatar = document.getElementById("avatar");
+const dateJoined = document.getElementById('date-joined');
+const bio = document.getElementById('bio');
+const repos = document.getElementById("repos");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const city = document.getElementById("city");
+const website = document.getElementById("website");
+const twitter = document.getElementById("twitter");
+const company = document.getElementById("company");
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-/*
+function setDarkModeProperties() {
+  root.setProperty('--lm-bg', '#141D2F');
+  root.setProperty('--lm-bg-content', '#1E2A47');
+  root.setProperty('--lm-text', 'white');
+  root.setProperty('--lm-text-alt', 'white');
+  root.setProperty('--lm-shadow-xl', 'rgba(70,88,109,0.15)');
+  root.setProperty('--lm-bio-text', '#FFFFFF');
+  root.setProperty('--lm-dev-text', '#FFFFFF');
+  toggleText.innerText = 'LIGHT';
+  icon.src = './images/icon-sun.svg';
+  root.setProperty('--lm-icon-bg', 'brightness(1000%)');
+}
+
+function setLightModeProperties() {
+  root.setProperty('--lm-bg', '#F6F8FF');
+  root.setProperty('--lm-bg-content', '#FEFEFE');
+  root.setProperty('--lm-text', '#4B6A9B');
+  root.setProperty('--lm-text-alt', '#2B3442');
+  root.setProperty('--lm-shadow-xl', 'rgba(70, 88, 109, 0.25)');
+  root.setProperty('--lm-bio-text', '#4B6A9B');
+  root.setProperty('--lm-dev-text', '#222731');
+  toggleText.innerText = 'DARK';
+  icon.src = './images/icon-moon.svg';
+  root.setProperty('--lm-icon-bg', 'brightness(100%)');
+}
+
+// Pick light or dark mode depending on system preferences
 if (prefersDarkScheme.matches) {
   document.body.classList.add("dark-theme");
+  setDarkModeProperties();
 } else {
   document.body.classList.remove("dark-theme");
-}
-*/
-//variables
-
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-const searchbar = document.querySelector('.form-control');
-const content = document.querySelector('content');
-const root = document.documentElement.style;
-const get = (param)=> document.getElementById('${param}');
-const url = "https://api.github.com/users/:username";
-const noResults = get ('no-results');
-const toggle = get ("toggle-btn");
-const toggleText = get ("toggle-text");
-const icon = get ("icon");
-const submit = get ("submit");
-const input = get ("input");
-const userName = get ("prf-name");
-const user = get ("user");
-const avatar = get ("avatar");
-const date = get ("date");
-const bio = get ("bio");
-const repos = get ("repos");
-const followers = get ("followers");
-const following = get ("following");
-const city = get ("city");
-const website = get ("website");
-const twitter = get ("twitter");
-const company = get ("company");
-const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
-const darkMode = false; 
-
-
-
-/*const btn = document.querySelector(".btn-toggle");
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-*/
-
-/*
-const currentTheme = localStorage.getItem("theme")
-if (currentTheme == "dark") {
-  document.body.classList.toggle("dark-theme");
-} 
-
-else if (currentTheme == "light") {
-  document.body.classList.toggle("light-theme");
+  setLightModeProperties();
 }
 
-toggle.addEventListener("click", function () {
-  if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light-theme");
-    var theme = document.body.classList.contains("light-theme")
-      ? "light"
-      : "dark";
-  } 
+// Read the theme from the local storage
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme == 'dark') {
+  document.body.classList.toggle('dark-theme');
+  setDarkModeProperties();
+} else {
+  document.body.classList.toggle('light-theme');
+  setLightModeProperties();
+}
 
-  else {
-    document.body.classList.toggle("dark-theme");
-    var theme = document.body.classList.contains("dark-theme")
-      ? "dark"
-      : "light";
+// Change the theme on the button click
+toggleDarkModeButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+  const theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+  if (theme === 'dark') {
+    setDarkModeProperties();
+  } else {
+    setLightModeProperties();
   }
-
-  localStorage.setItem("theme", theme);
+  localStorage.setItem('theme', theme);
 });
 
-*/
-
-
-
-
-//btns
-submit.addEventListener('click', function(){
-
-  if (input.value !== ""){
-      getUserData(url+input.value);
+// Submit form
+submit.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (input.value.trim() !== '') {
+    await getUserData(input.value);
   }
 })
 
-input.addEventListener("keydown", function(e) {
+input.addEventListener('keydown', async (e) => {
 
-  if (!e) { 
-      var e = window.event; 
+  if (!e) {
+    var e = window.event;
   }
 
-  if (e.key == "Enter") { 
-      if (input.value !== ""){
-          getUserData(url+input.value);
-      }
+  if (e.key == "Enter" && input.value.trim() !== '') {
+    await getUserData(input.value);
   }
 }, false);
 
-input.addEventListener('input', function(){
+input.addEventListener('input', function () {
 
   noResults.style.display = "none"
   searchbar.classList.remove('active')
   searchbar.classList.add('active')
 })
-toggle.addEventListener('click', function(){
-
-  if(darkMode == false){
-      darkModeProperties()
-  }
-
-  else{
-      lightModeProperties()
-  }
-})  
 
 //functions
-function getUserData(gitUrl){
-  fetch(gitUrl)
-  .then(response => response.json())
-  .then(data => {
-      updateProfile(data)
-  })
-  .catch(error => {
-      throw error;
-  })
+async function getUserData(githubUsername) {
+  const response = await fetch(baseUrl + githubUsername);
+  const data = await response.json();
+  updateProfile(data);
 }
-function updateProfile (data){
-  if(data.message !== "Not Found"){
-      noResults.style.display = "none";
-      function checkNull(param1, param2) {
-          if((param1 === "") || (param1 === null)){
-              param2.style.opacity = 0.5;
-              param2.previousElementSibling.style.opacity = 0.5;
-              return "Not available" 
-          }
-          else{
-              return `${param1}`
-          }
-      }
-      avatar.src = `${data.avatar_url}`
-      userName.innerText = `${data.name}`
-      user.innerText = `@${data.login}`
-      datesegments = data.created_at.split("T").shift().split("-")
-      date.innerText = `Joined ${datesegments[2]} ${months[datesegments[1]-1]} ${datesegments[0]}`
-      bio.innerText = (data.bio == null)? "This profile has no bio" : `${data.bio}`
-      repos.innerText = `${data.public_repos}`
-      followers.innerText = `${data.followers}`
-      following.innerText = `${data.following}`
-      city.innerText = checkNull(data.city, city)
-      website.innerText = checkNull(data.blog, website)
-      twitter.innerText = checkNull(data.twitter, twitter)
-      company.innerText = checkNull(data.company, company)
-      searchbar.classList.toggle('active')
-      content.classList.toggle('active')
+
+function checkNull(param1, param2) {
+  if ((param1 === "") || (param1 === null)) {
+    param2.style.opacity = 0.5;
+    param2.previousElementSibling.style.opacity = 0.5;
+    return "Not available"
   }
-  else{
-      noResults.style.display = "block";
+  else {
+    param2.style.opacity = 1;
+    return param1
   }
 }
-//dark mode default
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  darkModeProperties()
-  
+
+function updateProfile(data) {
+  if (data.message !== 'Not Found') {
+    noResults.style.display = "none";
+
+    avatar.src = data.avatar_url;
+    fullname.innerText = data.name;
+    username.innerText = `@${data.login}`;
+    datesegments = data.created_at.split("T").shift().split("-");
+    dateJoined.innerText = `Joined ${datesegments[2]} ${months[datesegments[1] - 1]} ${datesegments[0]}`
+    bio.innerText = (data.bio == null) ? "This profile has no bio" : `${data.bio}`
+    repos.innerText = `${data.public_repos}`
+    followers.innerText = `${data.followers}`
+    following.innerText = `${data.following}`
+    city.innerText = checkNull(data.location, city)
+    website.innerText = checkNull(data.blog, website)
+    twitter.innerText = checkNull(data.twitter_username, twitter)
+    company.innerText = checkNull(data.company, company)
+    searchbar.classList.toggle('active')
+    content.classList.toggle('active')
+  }
+  else {
+    noResults.style.display = "block";
+  }
 }
-function darkModeProperties(){
-  root.setProperty('--lm-bg', '#141D2F')
-  root.setProperty('--lm-bg-content', '#1E2A47')
-  root.setProperty('--lm-text', 'white')
-  root.setProperty('--lm-text-alt', 'white')
-  root.setProperty('--lm-shadow-xl', 'rgba(70,88,109,0.15)')
-  toggleText.innerText = "LIGHT"
-  icon.src = "/images/icon-sun.svg"
-  root.setProperty('--lm-icon-bg', 'brightness(1000%)')
-  darkMode = true
-}
-function lightModeProperties(){
-  root.setProperty('--lm-bg', '#F6F8FF')
-  root.setProperty('--lm-bg-content', '#FEFEFE')
-  root.setProperty('--lm-text', '#4B6A9B')
-  root.setProperty('--lm-text-alt', '#2B3442')
-  root.setProperty('--lm-shadow-xl', 'rgba(70, 88, 109, 0.25)')
-  toggleText.innerText = "DARK"
-  icon.src = "./images/icon-moon.svg"
-  root.setProperty('--lm-icon-bg', 'brightness(100%)')
-  darkMode = false
-}
-content.classList.toggle('active')
-searchbar.classList.toggle('active')
-getUserData(url+"octocat");
+
+// Get initial user (Github Octocat)
+getUserData('octocat');
